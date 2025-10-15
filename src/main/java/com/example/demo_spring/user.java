ee6ebcda43bd6baa61703e942d32bebd528c14cd
Thong Nguyen <87821311+AliasThog@users.noreply.github.com>
2025-10-15 21:55:36 +0700

    Update user.java

--- src/main/java/com/example/demo_spring/user.java
+++ src/main/java/com/example/demo_spring/user.java
@@ -2,6 +2,6 @@ package com.example.demo_spring;
 
 public class user {
     public static void main(String[] args) {
-        System.out.println("tuan");
+        System.out.println("phap");
     }
 }
